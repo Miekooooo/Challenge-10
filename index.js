a837c6827f8e1714e7c63272bba649c300fcf064
miekoooo <mocket17@gmail.com>
2023-09-05 18:36:44 -0500

    Shape classes started and imported to the index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import the classes
+const Triangle = require('./Triangle');
+const Circle = require('./Circle');
+const Square = require('./Square');
+
+// Create instances of the classes
+const triangle = new Triangle();
+const circle = new Circle();
+const square = new Square();
+
+// Use the instances and their methods
+// ...
